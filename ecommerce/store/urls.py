1814f0ee3c5d5df8cee131f13a01d2462527c568
sssava <santa03777@gmail.com>
2022-09-01 11:39:09 +0300

    comment ot out commit

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', store, name='store'),
+    path('cart/', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
+
+    path('update_item/', updateItem, name='update_item'),
+    path('process_order/', processOrder, name='process_order'),
+
+]
